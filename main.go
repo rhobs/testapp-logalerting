8800734d305222a04fdd66f80038ad0ca9a0a3d5
Nigel Smith <nigsmith@redhat.com>
2024-06-04 19:10:17 +0100

    Increase timeout

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ func main() {
 	sum := 0
 	for {
 		sum++ // repeated forever
-		time.Sleep(5 * time.Second)
+		time.Sleep(10 * time.Second)
 		logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 		logger.Debug("Nothing to worry about here")
-		logger.Info("Someone might want to look into this")
-		logger.Warn("Someone should probably look into this")
+		logger.Info("Testing logging alerting")
+		logger.Warn("Generating logs, lots of logs")
 		logger.Error("Panic")
 	}
 }
