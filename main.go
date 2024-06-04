ecf6c1161f26e1efec75e8afb0be2e54900f5c97
Nigel Smith <nigsmith@redhat.com>
2024-06-04 19:01:15 +0100

    lint

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "time"
-    "os"
-    "log/slog"   
+	"log/slog"
+	"os"
+	"time"
 )
 
 func main() {
 
-  sum := 0
-  for {
-	sum++ // repeated forever
-        time.Sleep(5 * time.Second)
-        logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-        logger.Debug("This is a Debug message")
-        logger.Info("This is a Info message")
-        logger.Warn("This is a Warning message")
-        logger.Error("This is a Error message")
-    }
+	sum := 0
+	for {
+		sum++ // repeated forever
+		time.Sleep(5 * time.Second)
+		logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+		logger.Debug("This is a Debug message")
+		logger.Info("This is a Info message")
+		logger.Warn("This is a Warning message")
+		logger.Error("This is a Error message")
+	}
 }
